6596749240233a8c2553332fc7b4a4f1f591a1f5
MartinLarrea <larreaamartin@gmail.com>
2020-11-30 00:02:16 -0500

    Spring Boot Project Added  -Controller for hello world created

---
+++ src/main/java/com/kruger/todo/KrugerTodoApplication.java
@@ -0,0 +1,13 @@
+package com.kruger.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KrugerTodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KrugerTodoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kruger/todo/KrugerTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kruger.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KrugerTodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
